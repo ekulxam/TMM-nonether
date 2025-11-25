9cb03ad91f7892f5a88d192f535780e6e9d8add3
SkyNotTheLimit <159592458+ekulxam@users.noreply.github.com>
2025-11-25 15:01:14 -0800

    Template go

---
+++ src/main/java/survivalblock/tmm_nonether/TMMNoNetherDataGenerator.java
@@ -0,0 +1,11 @@
+package survivalblock.tmm_nonether;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TMMNoNetherDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
